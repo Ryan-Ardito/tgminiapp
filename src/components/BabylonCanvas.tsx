f194f42b60af239191d1a7b7e1ebe245e16ce5a2
Ryan Ardito <ryan.m.ardito@gmail.com>
2024-12-11 19:45:31 -0700

    ui swing buttons

--- src/components/BabylonCanvas.tsx
+++ src/components/BabylonCanvas.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import "@babylonjs/loaders";
+import * as GUI from "@babylonjs/gui";
 
 import {
   Vector3,
@@ -9,6 +10,28 @@ import {
   SceneLoader,
   ArcRotateCamera,
 } from "@babylonjs/core";
+
+const SwingButton = (name: string, imageUrl: string): GUI.Button => {
+  const button = GUI.Button.CreateImageButton(name, "", imageUrl);
+
+  button.width = "100px";
+  button.height = "100px";
+  button.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+  button.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
+  button.color = "white";
+  button.background = "navy";
+  button.cornerRadius = 100;
+  button.thickness = 3;
+
+  const image = button.image!;
+  image.width = "60%";
+  image.height = "60%";
+  image.stretch = GUI.Image.STRETCH_UNIFORM;
+  image.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
+  image.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_CENTER;
+
+  return button;
+};
 
 const BabylonCanvas: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -48,6 +71,26 @@ const BabylonCanvas: React.FC = () => {
         "scenes/",
         "cricket_stadium.glb"
       );
+
+      const advancedTexture =
+        GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+
+      const swingLeftButton = SwingButton(
+        "swingLeft",
+        "images/cricketBatLeft.png"
+      );
+      swingLeftButton.top = "-50px";
+      swingLeftButton.left = "-100px";
+
+      const swingRightButton = SwingButton(
+        "swingRight",
+        "images/cricketBatRight.png"
+      );
+      swingRightButton.top = "-50px";
+      swingRightButton.left = "100px";
+
+      advancedTexture.addControl(swingLeftButton);
+      advancedTexture.addControl(swingRightButton);
 
       return scene;
     };
